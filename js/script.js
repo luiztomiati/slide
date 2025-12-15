5f11835238b4d9b6dcb058c178e7f51e9a2b88a8
Luiz Tomiati <luizfernandomello.dev@gmail.com>
2025-12-15 16:43:59 -0300

    Feita estilização do slide

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ import { SlideNav } from './slide.js';
 const slide = new SlideNav('.slide', '.wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-slide.addPag();
+slide.addPag('.custom-pag');
